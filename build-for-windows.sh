1eb56131cc32e6b4dbef8cb22d696b23a2f1d3a9
Paranoid-OS <du.brisingr.arget@gmail.com>
2020-11-29 14:34:34 +0000

    updated windows build script

--- build-for-windows.sh
+++ build-for-windows.sh
@@ -5,6 +5,10 @@ export LOCAL_LIB="`pwd`/lib"
 export LDFLAGS="-L$LOCAL_LIB/curl/lib/.libs -L$LOCAL_LIB/gmp/.libs -L$LOCAL_LIB/openssl"
 export CONFIGURE_ARGS="--with-curl=$LOCAL_LIB/curl --with-crypto=$LOCAL_LIB/openssl --host=x86_64-w64-mingw32"
 
+rm cpuminer/cpu-miner.c || echo done
+rm cpuminer/gmp.h || echo done
+
+ln -s `pwd`/cpu-miner.c `pwd`/cpuminer/cpu-miner.c
 
 cd cpuminer
 ln -s $LOCAL_LIB/gmp/gmp.h ./gmp.h
