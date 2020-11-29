92140de2408147b6ce988e0991926872c4576b49
Paranoid-OS <du.brisingr.arget@gmail.com>
2020-11-29 13:50:27 +0000

    debug build script + added Dependency list

---
+++ Dependencies.MD
@@ -0,0 +1,3 @@
+```
+sudo apt install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git libtool mingw-w64 libz-mingw-w64-dev
+```
--- build-for-windows.sh
+++ build-for-windows.sh
@@ -3,9 +3,11 @@
 cp -r dll release
 export LOCAL_LIB="`pwd`/lib"
 export LDFLAGS="-L$LOCAL_LIB/curl/lib/.libs -L$LOCAL_LIB/gmp/.libs -L$LOCAL_LIB/openssl"
-ln -s $LOCAL_LIB/gmp/gmp.h ./gmp.h
+export CONFIGURE_ARGS="--with-curl=$LOCAL_LIB/curl --with-crypto=$LOCAL_LIB/openssl --host=x86_64-w64-mingw32"
+
 
 cd cpuminer
+ln -s $LOCAL_LIB/gmp/gmp.h ./gmp.h
 
 make distclean || echo clean
 
@@ -13,7 +15,7 @@ rm -f config.status
 
 ./autogen.sh || echo done
 
-CFLAGS="-O3 -march=znver1 -Wall" ./configure "--with-curl=$LOCAL_LIB/curl --with-crypto=$LOCAL_LIB/openssl --host=x86_64-w64-mingw32"
+CFLAGS="-O3 -march=znver1 -Wall" ./configure $CONFIGURE_ARGS
 
 make -j 2
 
